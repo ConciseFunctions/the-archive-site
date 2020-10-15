d8068d56d9ba41def8fede0375cb56faad8eebe6
[Cursors] <cursors.dev@gmail.com>
2020-10-14 19:20:03 -0500

    Updates

---
+++ precache-manifest.70fe0ab34ae36c7ce7aea7d8c605e709.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d8e78c13f555bc2ef84c94364081ed9",
+    "url": "/the-archive-site/public/index.html"
+  },
+  {
+    "revision": "0d3ea6c405fc70e2db37",
+    "url": "/the-archive-site/public/static/css/main.20a5a292.chunk.css"
+  },
+  {
+    "revision": "f1afc1fa65bc5b1269a9",
+    "url": "/the-archive-site/public/static/js/2.c6eb3946.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/the-archive-site/public/static/js/2.c6eb3946.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d3ea6c405fc70e2db37",
+    "url": "/the-archive-site/public/static/js/main.3143f8b9.chunk.js"
+  },
+  {
+    "revision": "e79dbdf16f776f1b6242",
+    "url": "/the-archive-site/public/static/js/runtime-main.37bfe3da.js"
+  },
+  {
+    "revision": "bd69967b577f075ed12894077ddc07c9",
+    "url": "/the-archive-site/public/static/media/cog.bd69967b.png"
+  },
+  {
+    "revision": "861841d2d85d66d16787ab823ef868d7",
+    "url": "/the-archive-site/public/static/media/database.861841d2.png"
+  },
+  {
+    "revision": "12e1da6a1e3a0da7aa238a0ca665ca93",
+    "url": "/the-archive-site/public/static/media/docs.12e1da6a.png"
+  },
+  {
+    "revision": "2fd71d354dc1017b2149911b0281cc22",
+    "url": "/the-archive-site/public/static/media/moderation.2fd71d35.png"
+  }
+]);
